6958e24e64661c2874362a40b04f7c9062bfc7c8
saurav0171 <Kumar.saurabh0202@gmail.com>
2020-12-26 20:49:25 +0530

    testing 1

--- lib/UI/videoPlayer.dart
+++ lib/UI/videoPlayer.dart
@@ -67,7 +67,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
         duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
   }
 
-  //lovepreet
+  //lovepreet singh
   Future _scanQR() async {
     try {
       String qrResult = await BarcodeScanner.scan();
